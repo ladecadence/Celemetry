db59de3ac3bd12fa74dee395f6f43e8ea1d78a84
David Pello <zako@ladecadence.net>
2025-11-16 10:52:14 +0100

    Test for packet2 existing

--- tests/test.c
+++ tests/test.c
@@ -8,22 +8,22 @@ void main(void)
 
     if (packet)
     {
-    // add data
-    char* MISSION = "EKI2";
-    uint32_t mission_bytes = celemetry_base40_encode(MISSION);
-    celemetry_add_field(packet, CELEMETRY_ID, CELEMETRY_ID_BYTES, &mission_bytes);
+        // add data
+        char *MISSION = "EKI2";
+        uint32_t mission_bytes = celemetry_base40_encode(MISSION);
+        celemetry_add_field(packet, CELEMETRY_ID, CELEMETRY_ID_BYTES, &mission_bytes);
 
-    int16_t hdg = 100;
-    celemetry_add_field(packet, CELEMETRY_HDG, CELEMETRY_HDG_BYTES, &hdg);
+        int16_t hdg = 100;
+        celemetry_add_field(packet, CELEMETRY_HDG, CELEMETRY_HDG_BYTES, &hdg);
 
-    uint32_t data1 = 7890;
-    celemetry_add_field(packet, CELEMETRY_U32, CELEMETRY_U32_BYTES, &data1);
+        uint32_t data1 = 7890;
+        celemetry_add_field(packet, CELEMETRY_U32, CELEMETRY_U32_BYTES, &data1);
 
-    uint8_t data[] = {0x10, 0x11, 0x12, 0x13, 0x14, 0x15};
-    celemetry_add_field(packet, CELEMETRY_BLOB, sizeof(data), data);
+        uint8_t data[] = {0x10, 0x11, 0x12, 0x13, 0x14, 0x15};
+        celemetry_add_field(packet, CELEMETRY_BLOB, sizeof(data), data);
 
-    uint32_t data2 = 3456;
-    celemetry_add_field(packet, CELEMETRY_U32, CELEMETRY_U32_BYTES, &data2);
+        uint32_t data2 = 3456;
+        celemetry_add_field(packet, CELEMETRY_U32, CELEMETRY_U32_BYTES, &data2);
 
         // print it
         printf("Packet:  ");
@@ -48,36 +48,42 @@ void main(void)
         uint8_t decoded[CELEMETRY_PACKET_LEN];
         size_t dec_size = celemetry_cobs_decode(coded, enc_size, decoded);
         celemetry_packet_t *packet2 = celemetry_new_packet_from_data(decoded, dec_size);
-        // print it
-        printf("Decoded: ");
-        for (size_t i = 0; i < packet2->size; i++)
+        if (packet2)
         {
-            printf("%.2x", packet2->data[i]);
-        }
-        printf("\n");
+            // print it
+            printf("Decoded: ");
+            for (size_t i = 0; i < packet2->size; i++)
+            {
+                printf("%.2x", packet2->data[i]);
+            }
+            printf("\n");
 
-        // ok, get fields
-        uint32_t packet_num;
-        if (celemetry_get_packet_number(packet, &packet_num) == CELEMETRY_OK) {
-            printf("Packet num: %d\n", packet_num);
-        }
+            // ok, get fields
+            uint32_t packet_num;
+            if (celemetry_get_packet_number(packet, &packet_num) == CELEMETRY_OK)
+            {
+                printf("Packet num: %d\n", packet_num);
+            }
 
-        char id[6];
-        if (celemetry_get_id(packet, id) == CELEMETRY_OK) {
-            printf("ID: %s\n", id);
-        }
+            char id[6];
+            if (celemetry_get_id(packet, id) == CELEMETRY_OK)
+            {
+                printf("ID: %s\n", id);
+            }
 
-        int16_t heading;
-        if(celemetry_get_hdg(packet, &heading) == CELEMETRY_OK) {
-            printf("Heading: %d\n", heading);
-        }
+            int16_t heading;
+            if (celemetry_get_hdg(packet, &heading) == CELEMETRY_OK)
+            {
+                printf("Heading: %d\n", heading);
+            }
 
-        uint32_t d2;
-        if(celemetry_get_u32(packet, &d2, 2) == CELEMETRY_OK) {
-            printf("Data2: %d\n", d2);
+            uint32_t d2;
+            if (celemetry_get_u32(packet, &d2, 2) == CELEMETRY_OK)
+            {
+                printf("Data2: %d\n", d2);
+            }
+            celemetry_free(packet2);
         }
-
         celemetry_free(packet);
-        celemetry_free(packet2);
     }
 }
