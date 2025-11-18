6d7b2c442d74e508405483a656cc364301aa2788
David Pello <dvpg@tuta.io>
2025-11-18 10:45:36 +0100

    Modify ssdv test for 128bytes ssdv packets

--- tests/ssdv.c
+++ tests/ssdv.c
@@ -13,23 +13,23 @@ void main(void) {
     celemetry_packet_t *packet2 = celemetry_new_packet(1001);
 
     if (packet1 && packet2) {
-        // open ssdv file and read buffer
+        // open ssdv file and read buffer (two packets)
         FILE* f = fopen(ssdv_file, "rb");
         if (f) {
             fread(buffer, 1, 256, f);
             fclose(f);
         }
         // ok, now we need to split SSDV data in the two packets
-        celemetry_add_ssdv(packet1, 0, buffer);
-        celemetry_add_ssdv(packet2, 1, buffer+128);
+        celemetry_add_field(packet1, CELEMETRY_SSDV, CELEMETRY_SSDV_BYTES, buffer);
+        celemetry_add_field(packet2, CELEMETRY_SSDV, CELEMETRY_SSDV_BYTES, buffer+128);
 
         // now we need to get both parts
-        if (celemetry_get_ssdv(packet1, &ssdvpart, &ssdvbuf)==CELEMETRY_OK) {
+        if (celemetry_get_ssdv(packet1, &ssdvbuf)==CELEMETRY_OK) {
             FILE* fo = fopen(ssdv_out_file, "ab");
             fwrite(ssdvbuf, 1, 128, fo);
             fclose(fo);
         }
-        if (celemetry_get_ssdv(packet2, &ssdvpart, &ssdvbuf)==CELEMETRY_OK) {
+        if (celemetry_get_ssdv(packet2, &ssdvbuf)==CELEMETRY_OK) {
             FILE* fo = fopen(ssdv_out_file, "ab");
             fwrite(ssdvbuf, 1, 128, fo);
             fclose(fo);
